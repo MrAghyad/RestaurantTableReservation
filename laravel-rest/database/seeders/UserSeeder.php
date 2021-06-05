9914a49da5832e7405d2be7301c69e40a1719fce
MrAghyad <theriddler_214@hotmail.com>
2021-06-05 23:17:11 +0300

    Added 2 users to be seeded (admin and employee)

--- laravel-rest/database/seeders/UserSeeder.php
+++ laravel-rest/database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +14,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'id' => '1234',
+            'name' => 'admin_user',
+            'role' => 'admin',
+            'password' => bcrypt('123456'),
+        ]);
+
+        DB::table('users')->insert([
+            'id' => '5678',
+            'name' => 'employee_user',
+            'role' => 'employee',
+            'password' => bcrypt('123456'),
+        ]);
     }
 }
