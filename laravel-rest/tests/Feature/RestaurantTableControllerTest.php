3720707e6fc1bd201c5f0fd7bc8757d859d92d15
MrAghyad <theriddler_214@hotmail.com>
2021-06-05 23:37:40 +0300

    Initialized test class with seeders

--- laravel-rest/tests/Feature/RestaurantTableControllerTest.php
+++ laravel-rest/tests/Feature/RestaurantTableControllerTest.php
@@ -2,11 +2,32 @@
 
 namespace Tests\Feature;
 
+use Database\Seeders\RestaurantTableSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Database\Seeders\UserSeeder;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Config;
 
 class RestaurantTableControllerTest extends TestCase
 {
+    use RefreshDatabase;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        // Run the DatabaseSeeder...
+        $this->seed();
+
+        // Run a user seeder...
+        $this->seed(RestaurantTableSeeder::class);
+    }
+
+    private function seedUsers()
+    {
+        // Run the DatabaseSeeder...
+        $this->seed();
+
+        // Run a user seeder...
+        $this->seed(UserSeeder::class);
+    }
 }
