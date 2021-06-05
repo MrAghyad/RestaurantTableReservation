e7d483a8ff02acd16e4825324530656f14959df0
MrAghyad <theriddler_214@hotmail.com>
2021-06-06 00:05:30 +0300

    Added test_index_listing_tables_with_authorized_user_succeeds

--- laravel-rest/tests/Feature/RestaurantTableControllerTest.php
+++ laravel-rest/tests/Feature/RestaurantTableControllerTest.php
@@ -12,9 +12,8 @@ class RestaurantTableControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    protected function setUp(): void
+    private function seedRestaurantTables()
     {
-        parent::setUp();
         // Run the DatabaseSeeder...
         $this->seed();
 
@@ -30,4 +29,37 @@ class RestaurantTableControllerTest extends TestCase
         // Run a user seeder...
         $this->seed(UserSeeder::class);
     }
+
+#region test_index_listing_tables_with_authorized_user_succeeds
+
+    public function test_index_listing_tables_succeeds()
+    {
+        $this->seedRestaurantTables();
+        $this->seedUsers();
+
+        //login as admin
+        $baseUrl = Config::get('app.url') . '/api/v1/user/login';
+
+        $id = '1234';
+        $password = '123456';
+
+        $response = $this->postJson($baseUrl, [
+            'id' => $id,
+            'password' => $password
+        ]);
+
+        $token = json_decode($response->getContent())->token;
+
+        $baseUrl = Config::get('app.url') . '/api/v1/table';
+
+        $baseUrl = $baseUrl . '?token=' . $token;
+
+        $response = $this->getJson($baseUrl);
+
+        $response->assertStatus(200)
+        ->assertJsonStructure([
+            'msg', 'tables'
+        ]);
+    }
+#endregion
 }
